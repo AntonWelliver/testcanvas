dd688196c0d485bd89bd090b9ae2f68051cc7207
Your Name <you@example.com>
2018-10-29 10:32:31 +0100

    last changes to smiley

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ function draw() {
     var canvas = document.querySelector("canvas");
     var context = canvas.getContext("2d");
 
+    context.lineWidth = 3;
+    context.strokeStyle = "yellow";
 
     context.beginPath();
-    context.arc(100, 100, 50, 0, 2 * Math.PI);
-    context.moveTo(130, 80);
-
-    context.arc(120, 80, 10, 0, 2 * Math.PI);
-    context.moveTo(90, 80);
-
-    context.arc(80, 80, 10, 0, 2 * Math.PI);
-    context.moveTo(120, 120);
-
-    context.arc(100, 110, 22, 0, 1 * Math.PI);
+    drawHead(context);
+    drawEyes(context);
+    drawMouth(context);
     context.stroke();
-    
+
+    function drawHead(context){
+        context.arc(100, 100, 50, 0, 2 * Math.PI);
+        context.moveTo(130, 80);
+    }
+
+    function drawEyes(context){
+        context.arc(120, 80, 10, 0, 2 * Math.PI);
+        context.moveTo(90, 80);
+
+        context.arc(80, 80, 10, 0, 2 * Math.PI);
+        context.moveTo(120, 120);
+    }
+
+    function drawMouth(context){
+        context.arc(100, 110, 22, 0, 1 * Math.PI);
+    }
+
 }
